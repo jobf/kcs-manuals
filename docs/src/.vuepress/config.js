cf6ab7203210416b3f2f886817b0e7e7a036a94f
jf <j@b.f>
2020-11-14 00:19:19 +0000

    update sidebar config

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -47,24 +47,56 @@ module.exports = {
     sidebar: {
       '/guide/': [
         {
-          title: 'KCS 1.6 Manual',
-          collapsable: false,
+          title: 'Manuals',
           children: [
             '',
-            '02-track-mode-recording-tutorial',
-            '03-open-mode-editing-tutorial',
-            '04-open-and-track-mode-editing.md',
-            '05-song-mode-editing.md',
-            '06-track-mode-play-record',
-            '07-open-mode-play-record',
-            '08-song-mode-play',
-            '09-step-time-recording',
-            '10-load-save',
-            '11-set-options',
-            '12-midi',
-            '13-tips-for-advanced-users',
-            '14-troubleshooting',
-            '15-application-notes',
+            {
+              title: 'KCS 1.6 Manual',
+              path: '/guide/1.6-manual/',
+              children: [
+                '/guide/1.6-manual/',
+                '/guide/1.6-manual/02-track-mode-recording-tutorial',
+                '/guide/1.6-manual/03-open-mode-editing-tutorial',
+                '/guide/1.6-manual/04-open-and-track-mode-editing.md',
+                '/guide/1.6-manual/05-song-mode-editing.md',
+                '/guide/1.6-manual/06-track-mode-play-record',
+                '/guide/1.6-manual/07-open-mode-play-record',
+                '/guide/1.6-manual/08-song-mode-play',
+                '/guide/1.6-manual/09-step-time-recording',
+                '/guide/1.6-manual/10-load-save',
+                '/guide/1.6-manual/11-set-options',
+                '/guide/1.6-manual/12-midi',
+                '/guide/1.6-manual/13-tips-for-advanced-users',
+                '/guide/1.6-manual/14-troubleshooting',
+                '/guide/1.6-manual/15-application-notes',
+              ]
+            },
+            {
+              title: 'PVG and Master Edit',
+              path: '/guide/pvg-and-master-edit/',
+              children: [
+                '/guide/pvg-and-master-edit/',
+                '/guide/pvg-and-master-edit/01-understanding-the-pvg',
+                '/guide/pvg-and-master-edit/02-a-quick-overview-of-pvg-operations',
+                '/guide/pvg-and-master-edit/03-general-options',
+                '/guide/pvg-and-master-edit/04-protection',
+                '/guide/pvg-and-master-edit/05-restrictions',
+                '/guide/pvg-and-master-edit/06-defaults',
+                '/guide/pvg-and-master-edit/07-changes',
+                '/guide/pvg-and-master-edit/08-swap-copy',
+                '/guide/pvg-and-master-edit/09-set-values',
+                '/guide/pvg-and-master-edit/10-global-changes',
+                '/guide/pvg-and-master-edit/11-split-pattern',
+                '/guide/pvg-and-master-edit/12-in-betweens',
+                '/guide/pvg-and-master-edit/13-ornaments',
+                '/guide/pvg-and-master-edit/14-add-controllers',
+                '/guide/pvg-and-master-edit/15-vary-controllers',
+                '/guide/pvg-and-master-edit/16-get-store-load-save',
+                '/guide/pvg-and-master-edit/17-macros',
+                '/guide/pvg-and-master-edit/18-master-editor',
+                '/guide/pvg-and-master-edit/19-polyphonic-key-pressure',
+              ]
+            }
           ]
         }
       ],
